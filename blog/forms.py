1eab4e927e02b5421f0a696a8240f75f246a3445
swrko <inviiik@gmail.com>
2020-02-08 22:57:18 +0100

    fix registrovanie noveho usera - password

--- blog/forms.py
+++ blog/forms.py
@@ -9,13 +9,13 @@ class UserForm(ModelForm):
     password_repeat = forms.CharField(widget=forms.PasswordInput)
 
     def clean_password(self):
-        if self.cleaned_data.get('password') == self.cleaned_data.get('password_repeat'):
-            return True
+        if self.data.get('password') == self.data.get('password_repeat'):
+            return self.cleaned_data.get('password')
         raise ValidationError("Si kokot a hesla sa musia zhodovat")
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password','password_repeat']
 
 
 class LoginUserForm(Form):
